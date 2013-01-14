4d5290b03309c8ddd8a331f8b4e3c7bbd55fc0d9
Yogthos <dmitri.sotnikov@gmail.com>
2013-01-13 20:40:38 -0500

    removed unused dependencies

--- src/leiningen/new/compojure_app/home.clj
+++ src/leiningen/new/compojure_app/home.clj
@@ -1,8 +1,6 @@
 (ns {{name}}.routes.home
   (:use compojure.core)
-  (:require [compojure.handler :as handler]
-            [compojure.route :as route]
-            [{{name}}.views.layout :as layout]))
+  (:require [{{name}}.views.layout :as layout]))
 
 (defn home [] 
   (layout/common [:h1 "Hello World!"]))
