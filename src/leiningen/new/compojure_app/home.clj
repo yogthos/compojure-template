1995d792d06507610fc09c8f482860593ad4e142
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-04-28 13:20:39 -0300

    Update home.clj

--- src/leiningen/new/compojure_app/home.clj
+++ src/leiningen/new/compojure_app/home.clj
@@ -1,6 +1,6 @@
 (ns {{name}}.routes.home
-  (:use compojure.core)
-  (:require [{{name}}.views.layout :as layout]))
+  (:require [compojure.core :refer :all]
+            [{{name}}.views.layout :as layout]))
 
 (defn home [] 
   (layout/common [:h1 "Hello World!"]))
