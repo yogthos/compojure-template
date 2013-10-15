79d2fd3f70332d7df2a51c9659fa254e3b9af81a
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-10-15 09:25:48 -0400

    bumped up dependencies

--- src/leiningen/new/compojure_app/project.clj
+++ src/leiningen/new/compojure_app/project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [hiccup "1.0.3"]
-                 [ring-server "0.2.8"]]
-  :plugins [[lein-ring "0.8.5"]]
+                 [hiccup "1.0.4"]
+                 [ring-server "0.3.0"]]
+  :plugins [[lein-ring "0.8.7"]]
   :ring {:handler {{name}}.handler/war-handler
          :init {{name}}.handler/init
          :destroy {{name}}.handler/destroy}
@@ -14,4 +14,4 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring-mock "0.1.3"] [ring/ring-devel "1.1.8"]]}})
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.0"]]}})
