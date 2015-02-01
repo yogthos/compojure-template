53783b9ea2b711f207574b81ea274e1f42f439a4
Yury Antonov <yantonov@yandex.ru>
2015-02-01 17:08:48 +0500

    test fix

--- src/leiningen/new/compojure_app/handler_test.clj
+++ src/leiningen/new/compojure_app/handler_test.clj
@@ -7,7 +7,7 @@
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (.contains (:body response) "Hello World"))))
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
